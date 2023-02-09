4ede4433e7e5e308832b0e802a93eac4f1988185
Ziad Mansour <ziadmansour.4.9.2000@gmail.com>
2023-02-09 14:40:26 +0200

    🔧 REFACTR: services

---
+++ services/services/randomizer-service/server-two/main.go
@@ -0,0 +1,21 @@
+package main
+
+import (
+	"fmt"
+	"math/rand"
+	"net/http"
+
+	"github.com/gin-gonic/gin"
+)
+
+func getRandom(c *gin.Context) {
+	c.IndentedJSON(http.StatusOK, fmt.Sprintf("Server Two: %d", rand.Intn(100)))
+}
+
+func main() {
+	router := gin.Default()
+
+	router.GET("/", getRandom)
+
+	router.Run(":3000")
+}
